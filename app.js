ce6f43ca2a6305aaa0171f7d4b8ab04c18b8d5ed
liozen <Liozen@yandex.ru>
2024-08-25 14:21:16 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// JavaScript Document
+
+	const sutki=1000*60*60*24; // константа милисекунд в сутках
+	
+	var memory=[];
+	
+	// получить дату из строки
+	function str_tu_date(str){
+	    return new Date(str);
+	}
+	
+	function basic_calculation(d_value,coeff=1){
+		var d1=new Date(d_value);
+
+		var year=document.querySelector('#year_prigovor').value;
+		var month=document.querySelector('#month_prigovor').value;
+		var date_prigovor = new Date(document.querySelector('#date_prigovor').value);
+		if(date_prigovor==null) {
+			var d2=new Date(d1.toJSON());
+			d2.setFullYear(d1.getFullYear()+1*year);
+			d2.setMonth(d1.getMonth()+1*month);
+			var rez=Math.trunc((d2-d1)/sutki/coeff);
+		} else {
+			var d2=new Date(d1.toJSON());
+			d2.setFullYear(d1.getFullYear()+1*year);
+			d2.setMonth(d1.getMonth()+1*month);
+			var rez = Math.trunc((date_prigovor - d1)/sutki/coeff + (d2-d1)/sutki)
+		}
+		var d=new Date(d1.toJSON());
+		d.setDate(d.getDate() + rez);
+		
+		return d;
+	}
+	
+	function difference_date(d1,d2,coeff=1){
+	    return Math.trunc((d2-d1)/sutki/coeff);
+	}
+	
+	// добавить к дате лет, месяцев, дней
+	function add_tu_date(d,year=0,month=0,date=0){
+        d.setFullYear(d.getFullYear()+year);
+	    d.setMonth(d.getMonth()+month);
+	    d.setDate(d.getDate()+date);
+        return d;
+	}
